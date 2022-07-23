04ef2d4be83d139eea4fa1556ba660bff05914e1
nasirDoe <muhamad.nasir@saga.id>
2022-07-23 13:13:47 +0700

    fix: centering content

--- src/layouts/WithNavbar.vue
+++ src/layouts/WithNavbar.vue
@@ -3,7 +3,7 @@ import Navbar from '@components/partials/Navbar.vue';
 </script>
 
 <template>
-  <div class="w-full max-w-lg min-h-screen flex flex-col">
+  <div class="w-full mx-auto max-w-lg min-h-screen flex flex-col">
     <navbar />
     <main class="flex-1">
       <slot />
